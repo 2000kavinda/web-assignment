3391abef56177ca150b2051078089f5e478a3ad1
2000kavinda <98000159+2000kavinda@users.noreply.github.com>
2022-02-19 22:28:30 +0530

    login.php page upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>registration </title>
+</head>
+<body>
+<div class="container">
+	<div class="header">
+		<h2>Log In</h2>
+	</div>
+	<form action="login.php" method="post">
+		<div>
+			<lable for="username">Username</lable>
+			<input type="text" name="username" required>
+		</div>
+		<div>
+			<lable for="password">Password</lable>
+			<input type="password" name="password_1" required>
+		</div>
+		<button type="submit" name="login_user">Log In</button>
+		<p>Not a user ? <a href="registration.php"><b>Register Here</b></a></p>	
+	</form>
+	</div>
+</body>
+</html>
