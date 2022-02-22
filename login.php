bea55203c508572c4d6d91f525e502a03d4f8cff
2000kavinda <98000159+2000kavinda@users.noreply.github.com>
2022-02-22 15:23:33 +0530

    Update login.php

--- login.php
+++ login.php
@@ -10,6 +10,7 @@
 		<h2>Login</h2>
 	</div>
 	<form method="post" action="login.php">
+		<?php include('errors.php'); ?>
 		<div class="input-group">
 			<label>Username</label>
 			<input type="text" name="username">
